5c2beaf0d847f902ad597893474ce6cee876ab81
ozanyazici9 <ozanyazici9@gmail.com>
2026-07-15 18:38:27 +0300

    book repository

---
+++ Repositories/Contracts/IBookRepository.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Entities.Models;
+
+namespace Repositories.Contracts;
+
+public interface IBookRepository : IRepositoryBase<Book>
+{
+    IQueryable<Book> GetAllBooks(bool trackChanges);
+    IQueryable<Book> GetOneBook(int id, bool trackChanges);
+    void CreateOneBook(Book book);
+    void UpdateOneBook(Book book);
+    void DeleteOneBook(Book book);
+}
