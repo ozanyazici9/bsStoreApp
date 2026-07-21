8a07afce3a04b3b5755fb3b6810f8dc05eaea98a
ozanyazici9 <ozanyazici9@gmail.com>
2026-07-21 18:19:29 +0300

    auto mapper paket kurulum ve yapılandırma

--- bsStoreApp/Program.cs
+++ bsStoreApp/Program.cs
@@ -25,6 +25,8 @@ builder.Services.ConfigureSqlContext(builder.Configuration);
 builder.Services.ConfigureRepositoryManager();
 builder.Services.ConfigureServiceManager();
 builder.Services.ConfigureLoggerService();
+builder.Services.AddAutoMapper(typeof(Program));
+
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
