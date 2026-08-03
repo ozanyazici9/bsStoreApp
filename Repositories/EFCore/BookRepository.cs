b2d4a77d41b0a24de6a70083d6618c3b1970c82e
ozanyazici9 <ozanyazici9@gmail.com>
2026-08-03 17:01:29 +0300

    bad request exception, filter uygulaması, book repository extension

--- Repositories/EFCore/BookRepository.cs
+++ Repositories/EFCore/BookRepository.cs
@@ -5,7 +5,7 @@ using Repositories.Contracts;
 
 namespace Repositories.EFCore;
 
-public class BookRepository : RepositoryBase<Book>, IBookRepository
+public sealed class BookRepository : RepositoryBase<Book>, IBookRepository
 {
     public BookRepository(RepositoryContext context)
         : base(context) { }
@@ -19,10 +19,8 @@ public class BookRepository : RepositoryBase<Book>, IBookRepository
         bool trackChanges
     )
     {
-        var books = await FindByCondition(
-                b => (b.Price >= bookParameters.MinPrice) && (b.Price <= bookParameters.MaxPrice),
-                trackChanges
-            )
+        var books = await FindAll(trackChanges)
+            .FilterBooks(bookParameters.MinPrice, bookParameters.MaxPrice)
             .OrderBy(b => b.Id)
             .ToListAsync();
 
---
+++ Repositories/EFCore/BookRepositoryExtensions.cs
@@ -0,0 +1,12 @@
+using Entities.Models;
+
+namespace Repositories.EFCore;
+
+public static class BookRepositoryExtensions
+{
+    public static IQueryable<Book> FilterBooks(
+        this IQueryable<Book> books,
+        uint minPrice,
+        uint maxPrice
+    ) => books.Where(b => b.Price >= minPrice && b.Price <= maxPrice);
+}
