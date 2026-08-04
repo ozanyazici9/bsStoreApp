758fa956addf24f623bc03ae36d2e2c5f0d4e0e8
ozanyazici9 <ozanyazici9@gmail.com>
2026-08-04 17:10:14 +0300

    orderquerybuilder generic

--- Repositories/EFCore/Extensions/BookRepositoryExtensions.cs
+++ Repositories/EFCore/Extensions/BookRepositoryExtensions.cs
@@ -1,7 +1,5 @@
 using Entities.Models;
 using System.Linq.Dynamic.Core;
-using System.Reflection;
-using System.Text;
 
 namespace Repositories.EFCore.Extensions;
 
@@ -27,36 +25,12 @@ public static class BookRepositoryExtensions
         if (string.IsNullOrWhiteSpace(orderByQueryString))
             return books.OrderBy(b => b.Id);
 
-        var orderParams = orderByQueryString.Trim().Split(',');
-
-        var propertyInfos = typeof(Book).GetProperties(BindingFlags.Public | BindingFlags.Instance);
-
-        var orderQueryBuilder = new StringBuilder();
-
-        foreach (var param in orderParams)
-        {
-            if (string.IsNullOrWhiteSpace(param))
-                continue;
-            
-            var propertyFromQueryName = param.Split(' ')[0];
-
-            var objectProperty = propertyInfos.FirstOrDefault(pi => pi.Name.Equals(propertyFromQueryName, StringComparison.InvariantCultureIgnoreCase));
-
-            if (objectProperty is null)
-                continue;
-            
-            var direction = param.EndsWith(" desc") ? "descending" : "ascending";
-
-            orderQueryBuilder.Append($"{objectProperty.Name.ToString()} {direction},");
-        }
-
-        // title ascending, price descending, id ascending,
-        var orderQuery = orderQueryBuilder.ToString().TrimEnd(',', ' '); // sondaki virgülü ve boşluğu attık
+        
+        var orderQuery = OrderQueryBuilder.CreateOrderQuery<Book>(orderByQueryString);
 
         if (string.IsNullOrWhiteSpace(orderQuery))
             return books.OrderBy(b => b.Id);
 
         return books.OrderBy(orderQuery);
     }
-
 }
