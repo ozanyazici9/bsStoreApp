331c8d6aceef1b14bfb64dbccefa3185d51bf9ac
ozanyazici9 <ozanyazici9@gmail.com>
2026-08-04 16:31:49 +0300

    sort extension

--- Repositories/EFCore/BookRepository.cs
+++ Repositories/EFCore/BookRepository.cs
@@ -1,3 +1,4 @@
+using System.Collections.Immutable;
 using Entities.Models;
 using Entities.RequestFeatures;
 using Microsoft.EntityFrameworkCore;
@@ -23,7 +24,7 @@ public sealed class BookRepository : RepositoryBase<Book>, IBookRepository
         var books = await FindAll(trackChanges)
             .FilterBooks(bookParameters.MinPrice, bookParameters.MaxPrice)
             .Search(bookParameters.SearchTerm)
-            .OrderBy(b => b.Id)
+            .Sort(bookParameters.OrderBy)
             .ToListAsync();
 
         return PagedList<Book>.ToPagedList(
--- Repositories/EFCore/Extensions/BookRepositoryExtensions.cs
+++ Repositories/EFCore/Extensions/BookRepositoryExtensions.cs
@@ -1,4 +1,7 @@
 using Entities.Models;
+using System.Linq.Dynamic.Core;
+using System.Reflection;
+using System.Text;
 
 namespace Repositories.EFCore.Extensions;
 
@@ -18,4 +21,42 @@ public static class BookRepositoryExtensions
         var lowerCaseTerm = searchTerm.Trim().ToLower();
         return books.Where(b => b.Title.ToLower().Contains(searchTerm));
     }
+
+    public static IQueryable<Book> Sort(this IQueryable<Book> books, string orderByQueryString)
+    {
+        if (string.IsNullOrWhiteSpace(orderByQueryString))
+            return books.OrderBy(b => b.Id);
+
+        var orderParams = orderByQueryString.Trim().Split(',');
+
+        var propertyInfos = typeof(Book).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+        var orderQueryBuilder = new StringBuilder();
+
+        foreach (var param in orderParams)
+        {
+            if (string.IsNullOrWhiteSpace(param))
+                continue;
+            
+            var propertyFromQueryName = param.Split(' ')[0];
+
+            var objectProperty = propertyInfos.FirstOrDefault(pi => pi.Name.Equals(propertyFromQueryName, StringComparison.InvariantCultureIgnoreCase));
+
+            if (objectProperty is null)
+                continue;
+            
+            var direction = param.EndsWith(" desc") ? "descending" : "ascending";
+
+            orderQueryBuilder.Append($"{objectProperty.Name.ToString()} {direction},");
+        }
+
+        // title ascending, price descending, id ascending,
+        var orderQuery = orderQueryBuilder.ToString().TrimEnd(',', ' '); // sondaki virgülü ve boşluğu attık
+
+        if (string.IsNullOrWhiteSpace(orderQuery))
+            return books.OrderBy(b => b.Id);
+
+        return books.OrderBy(orderQuery);
+    }
+
 }
