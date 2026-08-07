d2d6ab94e7e47d4e8f978507744c6f5807d32369
ozanyazici9 <ozanyazici9@gmail.com>
2026-08-07 20:40:43 +0300

    URL ile api versiyonlama

--- Presentation/Controllers/BooksV2Controller.cs
+++ Presentation/Controllers/BooksV2Controller.cs
@@ -5,21 +5,24 @@ using Services.Contracts;
 namespace Presentation.Controllers;
 
 [ApiVersion("2.0")]
-[Route("api/books")]
 [ApiController]
+[Route("api/{v:apiversion}/books")]
 public class BooksV2Controller : ControllerBase
 {
-     private readonly IServiceManager _manager;
+    private readonly IServiceManager _manager;
 
-     public BooksV2Controller(IServiceManager manager)
-     {
-         _manager = manager;
-     }
+    public BooksV2Controller(IServiceManager manager)
+    {
+        _manager = manager;
+    }
 
-     [HttpGet]
-     public async Task<IActionResult> GetAllBooksAsync()
-     {
-         var books = await _manager.BookService.GetAllBooksAsync(trackChanges: false);
-         return Ok(books);
-     }
+    [HttpGet]
+    public async Task<IActionResult> GetAllBooksAsync()
+    {
+        var books = await _manager.BookService.GetAllBooksAsync(trackChanges: false);
+
+        var booksV2 = books.Select(b => new { Id = b.Id, Title = b.Title });
+
+        return Ok(booksV2);
+    }
 }
