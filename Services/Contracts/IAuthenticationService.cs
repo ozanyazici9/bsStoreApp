9536be9ae384663d6976fb772f62541a3a5bf057
ozanyazici9 <ozanyazici9@gmail.com>
2026-08-09 19:45:58 +0300

    IAuthenticationService

---
+++ Services/Contracts/IAuthenticationService.cs
@@ -0,0 +1,9 @@
+using Entities.DataTransferObjects;
+using Microsoft.AspNetCore.Identity;
+
+namespace Services.Contracts;
+
+public interface IAuthenticationService
+{
+    Task<IdentityResult> RegisterUser(UserForRegistrationDto userForRegistrationDto);
+}
