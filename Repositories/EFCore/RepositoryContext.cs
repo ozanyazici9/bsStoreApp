8a32f27ecf8e12c8e6047e2f45301337640b0160
ozanyazici9 <ozanyazici9@gmail.com>
2026-08-09 16:19:54 +0300

    ıdentity db context

--- Repositories/EFCore/RepositoryContext.cs
+++ Repositories/EFCore/RepositoryContext.cs
@@ -1,14 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Entities.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Repositories.EFCore.Config;
 
 namespace Repositories.EFCore;
 
-public class RepositoryContext : DbContext
+public class RepositoryContext : IdentityDbContext<User>
 {
     public RepositoryContext(DbContextOptions options)
         : base(options) { }
@@ -17,6 +14,7 @@ public class RepositoryContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        base.OnModelCreating(modelBuilder);
         modelBuilder.ApplyConfiguration(new BookConfig());
     }
 }
