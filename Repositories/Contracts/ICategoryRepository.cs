44f68f20cc1b5970050ff7a55ab676ea05741048
ozanyazici9 <ozanyazici9@gmail.com>
2026-08-21 18:13:01 +0300

    category repository

---
+++ Repositories/Contracts/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+using Entities.Models;
+
+namespace Repositories.Contracts;
+
+public interface ICategoryRepository : IRepositoryBase<Category>
+{
+    Task<IEnumerable<Category>> GetAllCategoriesAsync(bool trackChanges);
+    Task<Category> GetOneCategoryByIdAsync(int id, bool trackChanges);
+    void CreateOneCategory(Category category);
+    void DeleteOneCategory(Category category);
+    void UpdateOneCategory(Category category);
+}
